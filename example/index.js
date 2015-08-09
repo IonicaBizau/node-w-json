067414537dc282291a42f44dc086f9c5323e6a41
Ionică Bizău <bizauionica@yahoo.com>
2015-08-09 15:05:27 +0300

    Added the new_line options

--- example/index.js
+++ example/index.js
@@ -8,3 +8,9 @@ WriteJson(__dirname + "/test.json", { "hello": "world" }, function (err, data) {
 
 // Write the same file synchronously
 WriteJson(__dirname + "/test.json", { "hello": "mars" });
+
+// 4 space indent and new line at the end
+WriteJson(__dirname + "/test.json", { "hello": "mars" }, {
+    new_line: true
+  , space: 4
+});
